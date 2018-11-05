9b44ce485f81d7ec664db16f071af56451c030f0
netshellem <shellem@126.com>
2018-11-05 21:21:11 +0800

    update bootstrap-table-filter-control.js

--- src/main/java/hello/data/pojo/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package hello.data.pojo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface UserRoleRepository  extends CrudRepository<UserRole, Long> {
-    public List<UserRole> findByAppUser(AppUser appUser);
-}
