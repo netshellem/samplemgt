3a7cdd52d50729dd7350b7e5cf4c2971a76cc6bf
netshellem <shellem@126.com>
2018-11-17 16:23:28 +0800

    add origin data field

--- src/main/java/com/design/samplemgt/endpoint/SampleEndPoint.java
+++ src/main/java/com/design/samplemgt/endpoint/SampleEndPoint.java
@@ -51,6 +51,7 @@ public class SampleEndPoint {
         c.setModel(sample.model);
         c.setSample(sample.sample);
         c.setStatus(sample.status);
+        c.setOrigin(sample.origin);
         c.setEnabled(true);
         return clothService.Save(c);
     }
